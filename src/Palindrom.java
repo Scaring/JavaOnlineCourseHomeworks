3bc52f9e1afd9b3f720b411ca2d398dcf5628634
Dmytro Kuschevyy <scaring22@gmail.com>
2021-07-16 17:38:52 +0300

    task-4 sqrt

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,20 +1,18 @@
 public class Palindrom {
     public static void main(String[] args) {
-        int pal = 1001;
+        int pal = 101101;
         System.out.println(isPalindrome(pal));
 
     }
 
     public static boolean isPalindrome(int x) {
         boolean result = true;
-
-        Integer j = x;
+        
+        Integer j = (Integer) x;
         String[] s = j.toString().split("");
         int stringLength = s.length;
 
-        for (int i = 0; i < stringLength; i++) {
-            if (i >= (stringLength - (i + 1)))
-                break;
+        for (int i = 0; i < stringLength / 2; i++) {
 
             int digitFromStart = Integer.parseInt(s[i]);
             int digitFromEnd = Integer.parseInt(s[stringLength - (1 + i)]);
---
+++ src/Sqrt.java
@@ -0,0 +1,21 @@
+public class Sqrt {
+    public static void main(String[] args) {
+        int num = 16;
+        System.out.println(mySqrt(num));
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 1)
+            return 1;
+
+        if (x == Integer.MAX_VALUE)
+            return 46340;
+
+        for (int i = 1; true ; i++) {
+            if (x / (i * i) != 0)
+                continue;
+            i -= 1;
+            return i;
+        }
+    }
+}
