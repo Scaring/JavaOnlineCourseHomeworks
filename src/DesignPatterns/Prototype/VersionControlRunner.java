138fa9f592a87b95df8da568404c74f9b4a413e1
Dmytro Kushchevyi <dkushc@softserveinc.com>
2021-11-23 15:07:35 +0200

    prototype

---
+++ src/DesignPatterns/Prototype/VersionControlRunner.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Prototype;
+
+public class VersionControlRunner {
+    public static void main(String[] args) {
+        Project master = new Project(1, "Super Project", "SourceCode code = new SourceCode()");
+        System.out.println(master);
+        System.out.println("\n================================\n");
+
+//        Project masterCopy = (Project) master.copy();
+//        System.out.println(masterCopy);
+
+        ProjectFactory factory = new ProjectFactory(master);
+        Project masterClone = factory.cloneProject();
+        System.out.println(masterClone);
+    }
+}
